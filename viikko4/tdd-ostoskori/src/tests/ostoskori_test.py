454249a45c2586c33e73cf60cc97af03f00c29df
Sampo <57003465+realtalin@users.noreply.github.com>
2022-11-28 00:11:26 +0200

    Kahden saman tuotteen lisäämisen jälkeen ostoskori sisältää ostoksen jolla sama nimi kuin tuotteella ja lukumäärä 2

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -77,6 +77,15 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(len(ostokset), 1)
 
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_kori_sisaltaa_ostosolion_jolla_oikea_nimi_ja_lukumaara(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
+
+        ostos = self.kori.ostokset()[0]
+
+        self.assertEqual(ostos.tuotteen_nimi(), "Maito")
+        self.assertEqual(ostos.lukumaara(), 2)
+
         
 
 
