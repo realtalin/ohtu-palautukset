5a80cde16aa897145e0855519370e04cfc0b1e3b
Sampo <57003465+realtalin@users.noreply.github.com>
2022-11-27 23:41:03 +0200

    Lisää kaksi eri tuottetta koriin

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -6,6 +6,7 @@ class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
         self.maito = Tuote("Maito", 3)
+        self.kahvi = Tuote("Kahvi", 10)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
@@ -20,3 +21,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.maito)
 
         self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.kahvi)
+
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
