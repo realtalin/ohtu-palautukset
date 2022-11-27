c7500aee79b9abf38f71029f8335ee0f8f04a845
Sampo <57003465+realtalin@users.noreply.github.com>
2022-11-27 23:47:34 +0200

    Korjaa väärin luettu testi (sama -> eri)

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -28,14 +28,16 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
-    def test_kahden_saman_tuoteen_lisaamisen_jalkeen_korin_hinta_on_tavaroiden_yhteishinta(self):
+    def test_kahden_eri_tuoteen_lisaamisen_jalkeen_korin_hinta_on_tavaroiden_yhteishinta(self):
         self.kori.lisaa_tuote(self.maito)
-        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.kahvi)
 
-        self.assertEqual(self.kori.hinta(), 6)
+        self.assertEqual(self.kori.hinta(), 13)
 
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
         self.kori.lisaa_tuote(self.maito)
         self.kori.lisaa_tuote(self.maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+
