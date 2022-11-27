b1c1be1df335bb0b5ef411458065b447ba02eb66
Sampo <57003465+realtalin@users.noreply.github.com>
2022-11-27 23:42:57 +0200

    Korin hinta on kahden saman lisätyn tuotteen yhteishinta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -27,3 +27,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.kahvi)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_saman_tuoteen_lisaamisen_jalkeen_korin_hinta_on_tavaroiden_yhteishinta(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
+
+        self.assertEqual(self.kori.hinta(), 6)
