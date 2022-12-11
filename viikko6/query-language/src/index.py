fb3f40c21d45c04a9ee8de19481f47f5c0cbfa06
Sampo <57003465+realtalin@users.noreply.github.com>
2022-12-11 04:13:23 +0200

    Tee tehtävä 5

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,17 +1,31 @@
 from statistics import Statistics
 from player_reader import PlayerReader
 from matchers import And, HasAtLeast, PlaysIn
+from query import QueryBuilder
+
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(5, "assists"),
-        PlaysIn("PHI")
-    )
+    query = QueryBuilder()
+
+    matcher = (
+        query
+            .oneOf(
+                query
+                    .playsIn("PHI")
+                    .hasAtLeast(10, "assists")
+                    .hasFewerThan(5, "goals")
+                    .build(),
+                query
+                    .playsIn("EDM")
+                    .hasAtLeast(50, "points")
+                    .build()
+            )
+            .build()
+        )
 
     for player in stats.matches(matcher):
         print(player)
--- viikko6/query-language/src/query.py
+++ viikko6/query-language/src/query.py
@@ -1,4 +1,4 @@
-from matchers import All, PlaysIn, HasAtLeast, HasFewerThan, And
+from matchers import All, PlaysIn, HasAtLeast, HasFewerThan, And, Or
 
 class QueryBuilder:
     def __init__(self, query=All()):
@@ -12,6 +12,9 @@ class QueryBuilder:
 
     def hasFewerThan(self, value, attr):
         return QueryBuilder(And(self._query, HasFewerThan(value, attr)))
+
+    def oneOf(self, *queries):
+        return QueryBuilder(And(self._query, Or(*queries)))
 
     def build(self):
         return self._query
